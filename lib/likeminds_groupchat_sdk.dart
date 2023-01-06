b83c68e5b3f8f6b6aedf4237a81477d1f5fd4cc8
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-01-06 14:54:20 +0530

    Creating models for SDK
    Client models required for running SDK

---
+++ lib/likeminds_groupchat_sdk.dart
@@ -0,0 +1,10 @@
+library likeminds_groupchat_sdk;
+
+import 'package:likeminds_groupchat_sdk/models/sdk/init_extra_model.dart';
+
+/// A Calculator.
+class LikeMindsGroupChatSDK {
+  void initiateLikeMinds(InitiateLikeMindsChatExtra extras) {
+    //Implement initiate call
+  }
+}
---
+++ test/likeminds_groupchat_sdk_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:likeminds_groupchat_sdk/likeminds_groupchat_sdk.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
