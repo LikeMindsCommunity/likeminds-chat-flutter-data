9f11cda8425bf25ff3290b3e8a23a549fc5f0c49
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-01-10 14:49:30 +0530

    Following package structure

--- lib/likeminds_groupchat.dart
+++ lib/likeminds_groupchat.dart
@@ -1,12 +1,18 @@
 library likeminds_groupchat_sdk;
 
-import 'package:likeminds_groupchat/models/sdk/init_extra_model.dart';
+import 'package:likeminds_groupchat/src/models/sdk/init_extra_model.dart';
 
 /// The starting class of the SDK
 class LikeMindsGroupChat {
   void initiateLikeMinds(InitiateLikeMindsChatExtra extras) {
     //Implement initiate call
   }
+
+  void logout() {}
+
+  void initiateGroupChat() {}
+
+  void parseDeepLink() {}
 }
 
 final InitiateLikeMindsChatExtra extras = InitiateLikeMindsChatExtra(
