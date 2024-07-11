93d1517ab83065fbf230734a7c264627d86777cf
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2024-07-11 17:37:33 +0530

    Formatting changes

--- lib/src/services/explore_service.dart
+++ lib/src/services/explore_service.dart
@@ -1,27 +1,3 @@
-// getExploreTabCount(GetExploreTabCountResponse? -> Unit)
-
-// Call community/member/home/meta with GET method to get explore tab count.
-
-// if community/member/home/meta success==false
-
-// returngetExploreTabCountResponse:{success=false, errorMessage:<returned in community/member/home/meta>}
-
-// else
-
-// return getExploreTabCountResponse:{success=true, LMResponse<GetExploreTabCountResponse>}
-
-// getExploreFeed(GetExploreFeedRequest, GetExploreFeedResponse? -> Unit)
-
-// Call community/feed with GET method and with x-accept-version=v2(Headers), page, orderType, pinned received in GetExploreFeedRequest to get explore feed chatrooms
-
-// If community/feed success==false
-
-// return getExploreFeedResponse: {success: false, errorMessage: <returned in community/feed>,  initiateResponse: <returned in initiate()>}
-
-// else
-
-// return getExploreFeedResponse: {success: true, LMResponse<GetExploreFeedResponse>}
-
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:likeminds_chat_fl/src/managers/api/api_manager.dart';
@@ -45,16 +21,16 @@ class ExploreService extends IExploreService {
       getExploreTabCount() async {
     try {
       final response = await apiManager.client().get(
-        // community/member/home/meta
-        apiManager.endPoints.exploreTabCountEndpoint,
-      );
+            // community/member/home/meta
+            apiManager.endPoints.exploreTabCountEndpoint,
+          );
       if (!response.data['success'] || response.data['data'] == null) {
         return LMResponse.error(
           errorMessage: response.data['error_message'] ?? 'An error occurred',
         );
       }
       GetExploreTabCountResponseEntity getExploreTabCountResponse =
-          GetExploreTabCountResponseEntity.fromJson(response.data);
+          GetExploreTabCountResponseEntity.fromJson(response.data['data']);
       return LMResponse.success(data: getExploreTabCountResponse);
     } on DioException catch (e) {
       debugPrint(e.message);
@@ -67,15 +43,15 @@ class ExploreService extends IExploreService {
       GetExploreFeedRequest request) async {
     try {
       final response = await apiManager.client().get(
-        // community/feed
-        apiManager.endPoints.exploreFeedEndpoint,
-        queryParameters: request.toJson(),
-        options: Options(
-          headers: {
-            'x-accept-version': 'v2',
-          },
-        ),
-      );
+            // community/feed
+            apiManager.endPoints.exploreFeedEndpoint,
+            queryParameters: request.toJson(),
+            options: Options(
+              headers: {
+                'x-accept-version': 'v2',
+              },
+            ),
+          );
 
       if (!response.data['success'] || response.data['data'] == null) {
         return LMResponse.error(
